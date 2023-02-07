d55ea2acc714dc71af29dae667653fd6912ea191
Rodney Olid <olidrodney@gmail.com>
2023-02-07 13:26:06 -0500

    GameMenu made and basic interface

--- src/GameMenu.java
+++ src/GameMenu.java
@@ -1,6 +1,23 @@
+import java.util.*;
 public class GameMenu {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+    public static void start() {
+        int choice = 0;
+        Scanner input = new Scanner(System.in);
+        while (choice != 3) {
+            System.out.println("Welcome to *Farm Game*");
+            System.out.println("1. Start\n2. Rules\n3. Quit");
+            choice = input.nextInt();
+            if (choice == 1) {
+                /* start game code */
+            } else if (choice == 2) {
+                System.out.println("""
+                        Your goal to reach a total profit of *$1000* the fastest you can. You can do that by
+                        growing plants and animals, then selling them at the market. You can also buy items you may need at
+                        the market as well.""");
+                System.out.println("");
+                choice = 0;
+            }
+        }
+        System.out.println("Thanks for playing!");
     }
 }
